bce10867b08e04e66d925f16ccb15c8417f6f2a6
Tim Kelley <timkelley683@gmail.com>
2023-01-26 17:43:50 -0800

    created error handlers

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+
+const postRoutes = require("./routes/posts.js");
+const authRoutes = require("./routes/auth.js");
+const userRoutes = require("./routes/users.js");
+
+const app = express();
+
+
+app.use(express.json())
+app.use(cors());
+
+app.use("/posts", postRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+
+
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+    return next(new NotFoundError());
+  });
+  
+  /** Generic error handler; anything unhandled goes here. */
+  app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
+  
+    return res.status(status).json({
+      error: { message, status },
+    });
+  });
+
+module.exports = app;
