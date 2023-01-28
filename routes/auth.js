1b01986b18f9523b8edaa68c849da2bd4346835a
Tim Kelley <timkelley683@gmail.com>
2023-01-27 18:42:30 -0800

    json verification

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const db = require("../db.js");
 const bcrypt = require("bcrypt");
+const User = require("../models/user")
+const { createToken } = require("../helpers/tokens")
 
 const router = express.Router()
 
@@ -8,35 +10,86 @@ router.get("/", (req, res) => {
     res.json("AUTH")
 });
 
-router.post("/register", (req, res) => {
-    const q = "SELECT * FROM users WHERE email = ? OR username = ?"
+// router.post("/register", (req, res) => {
+//     const q = "SELECT * FROM users WHERE email = ? OR username = ?"
 
-    db.query(q, [req.body.email, req.body.username], (err,data) => {
-        if(err) return res.json(err)
+//     db.query(q, [req.body.email, req.body.username], (err,data) => {
+//         if(err) return res.json(err)
 
-        if(data.length) return res.status(409).json("User already exists.");
+//         if(data.length) return res.status(409).json("User already exists.");
 
-        //hash pw and create user
-        const salt = bcrypt.genSaltSync(10);
-        const hash = bcrypt.hashSync(req.body.password, salt);
+//         //hash pw and create user
+//         const salt = bcrypt.genSaltSync(10);
+//         const hash = bcrypt.hashSync(req.body.password, salt);
 
-        const qu = `INSERT INTO users
-                        ('username', 'email', 'password')
-                        VALUES (?)`
-        const values = [
-            req.body.username,
-            req.body.email,
-            hash,
-        ]
+//         const qu = `INSERT INTO users
+//                         ('username', 'email', 'password')
+//                         VALUES (?)`
+//         const values = [
+//             req.body.username,
+//             req.body.email,
+//             hash,
+//         ]
 
-        db.query(qu, [values], (err, data) => {
-            if(err) return res.json(err);
+//         db.query(qu, [values], (err, data) => {
+//             if(err) return res.json(err);
 
-            return res.status(200).json("User successfully created.");
-        })
+//             return res.status(200).json("User successfully created.");
+//         })
                         
-    })
-})
+//     })
+// })
+
+/** POST /auth/register:   { user } => { token }
+ *
+ * user must include { username, password, firstName, lastName, email }
+ *
+ * Returns JWT token which can be used to authenticate further requests.
+ *
+ * Authorization required: none
+ */
+
+ router.post("/register", async function (req, res, next) {
+    try {
+    //   const validator = jsonschema.validate(req.body, userRegisterSchema);
+    //   if (!validator.valid) {
+    //     const errs = validator.errors.map(e => e.stack);
+    //     throw new BadRequestError(errs);
+    //   }
+  
+      const newUser = await User.register({ ...req.body });
+      const token = createToken(newUser);
+      return res.status(201).json({ token });
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+
+  /** POST /auth/token:  { username, password } => { token }
+ *
+ * Returns JWT token which can be used to authenticate further requests.
+ *
+ * Authorization required: none
+ */
+
+router.post("/token", async function (req, res, next) {
+    try {
+    //   const validator = jsonschema.validate(req.body, userAuthSchema);
+    //   if (!validator.valid) {
+    //     const errs = validator.errors.map(e => e.stack);
+    //     throw new BadRequestError(errs);
+    //   }
+  
+      const { username, password } = req.body;
+      const user = await User.authenticate(username, password);
+      const token = createToken(user);
+      return res.json({ token });
+    } catch (err) {
+      return next(err);
+    }
+  });
+  
 
 // router.post("/login", (req, res) => {
 
