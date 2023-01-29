87df16fb11f2dabd22492fb10120ce044b44d68d
Tim Kelley <timkelley683@gmail.com>
2023-01-28 18:50:23 -0800

    added logout

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 const express = require("express");
-
+const User = require("../models/user");
 const router = express.Router()
 
 router.get("/", (req, res) => {
     res.json("USERS")
 });
+
+/** GET /[username] => { user }
+ *
+ * Returns { username, firstName, lastName, isAdmin, jobs }
+ *   where jobs is { id, title, companyHandle, companyName, state }
+ *
+ * Authorization required: admin or same user-as-:username
+ **/
+
+ router.get("/:username", async function (req, res, next) {
+    try {
+      const user = await User.get(req.params.username);
+      return res.json({ user });
+    } catch (err) {
+      return next(err);
+    }
+  });
 
 module.exports = router;
