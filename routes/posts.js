1cdc7b75af11322a6fe9394e18c7a189a03f8289
Tim Kelley <timkelley683@gmail.com>
2023-02-02 18:17:22 -0800

    add single post and finished post by category

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { addPost } = require("../controllers/post.js");
 const Post = require("../models/post.js");
-
+const db = require("../db");
+const { NotFoundError } = require("../expressError.js");
 
 const router = express.Router()
 
@@ -9,25 +10,89 @@ const router = express.Router()
 //     res.json("this works now")
 // });
 
+// router.get("/", async (req, res, next) => {
+//     // const q = req.query.cat
+//     try {
+//         // const validator = jsonschema.validate(q, companySearchSchema);
+//         // if (!validator.valid) {
+//         //   const errs = validator.errors.map(e => e.stack);
+//         //   throw new BadRequestError(errs);
+//         // }
+        
+//         const posts = await Post.getAllPosts();
+//         return res.json({ posts });
+//       } catch (err) {
+//         return next(err);
+//       }
+// });
+
+//Trial #1
+// router.get("/", async (req, res, next) => {
+//     try{
+//         const q = req.query.cat 
+//         ? "SELECT * FROM posts WHERE cat = $1::text"
+//         : "SELECT * FROM posts"
+//         console.log("Q", q)
+//         const postRes = await db.query(q, [req.query.cat]);
+//         console.log("POSTRES", postRes)
+//         const posts = postRes.rows;
+
+//         if (!posts) throw new NotFoundError(`No posts under ${q}`)
+
+//         return posts;
+//     }catch(err){
+//         return next(err);
+//     }
+// })
+
+
+//Trial #2
 router.get("/", async (req, res, next) => {
     // const q = req.query.cat
+
+    // if (q !== undefined){
+    //     const postsByCat = await Post.getPostsByCat();
+    //     console.log(postsByCat);
+    //     return res.json({ postsByCat });
+    // } else{
+    //     const posts = await Post.getAllPosts();
+    //     return res.json({ posts });
+    // }
     try {
         // const validator = jsonschema.validate(q, companySearchSchema);
         // if (!validator.valid) {
         //   const errs = validator.errors.map(e => e.stack);
         //   throw new BadRequestError(errs);
         // }
-        
-        const posts = await Post.getAllPosts();
-        return res.json({ posts });
+        const q = req.query.cat
+        console.log("query", req.query);
+        console.log("Query2", JSON.stringify(req.query.cat))
+        if (q !== undefined){
+            const postsByCat = await Post.getPostsByCat(q);
+            console.log("QUERY", q)
+            console.log("POSTCAT", postsByCat);
+            return res.json({ postsByCat });
+        } else{
+            const posts = await Post.getAllPosts();
+            console.log("ALLPOST", posts )
+            return res.json({ posts });
+        }
       } catch (err) {
         return next(err);
       }
 });
 
-router.get("/:id", (req, res) => {
-    
+
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const post = await Post.getPostById(req.params.id);
+        return res.json({ post });
+      } catch (err) {
+        return next(err);
+      }
 });
+
 
 router.post("/", (req, res) => {
     
