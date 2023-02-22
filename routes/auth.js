674b5f10cb21c9f304ec9337db4adc6ba78f48cf
Tim Kelley <timkelley683@gmail.com>
2023-02-22 15:53:11 -0800

    clean up code

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const db = require("../db.js");
-const bcrypt = require("bcrypt");
+
 const jsonschema = require("jsonschema");
 const User = require("../models/user")
 const { createToken } = require("../helpers/tokens");
@@ -9,40 +8,6 @@ const newUserSchema = require("../schemas/NewUserSchema.json")
 const userAuthSchema = require("../schemas/UserAuth.json")
 
 const router = express.Router()
-
-router.get("/", (req, res) => {
-    res.json("AUTH")
-});
-
-// router.post("/register", (req, res) => {
-//     const q = "SELECT * FROM users WHERE email = ? OR username = ?"
-
-//     db.query(q, [req.body.email, req.body.username], (err,data) => {
-//         if(err) return res.json(err)
-
-//         if(data.length) return res.status(409).json("User already exists.");
-
-//         //hash pw and create user
-//         const salt = bcrypt.genSaltSync(10);
-//         const hash = bcrypt.hashSync(req.body.password, salt);
-
-//         const qu = `INSERT INTO users
-//                         ('username', 'email', 'password')
-//                         VALUES (?)`
-//         const values = [
-//             req.body.username,
-//             req.body.email,
-//             hash,
-//         ]
-
-//         db.query(qu, [values], (err, data) => {
-//             if(err) return res.json(err);
-
-//             return res.status(200).json("User successfully created.");
-//         })
-                        
-//     })
-// })
 
 /** POST /auth/register:   { user } => { token }
  *
@@ -94,14 +59,5 @@ router.post("/token", async function (req, res, next) {
       return next(err);
     }
   });
-  
-
-// router.post("/login", (req, res) => {
-
-// })
-
-// router.post("/logout", (req, res) => {
-
-// })
 
 module.exports = router;
