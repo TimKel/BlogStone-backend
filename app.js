d75161f4f2a6b03ff746b960a9db3451cc934b3b
Tim Kelley <timkelley683@gmail.com>
2023-02-24 11:28:25 -0800

    prepare for deployment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
+
 const { NotFoundError } = require("./expressError.js");
 
 
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors());
-app.use(cookieParser());
+
 app.use(authenticateJWT);
 
 
